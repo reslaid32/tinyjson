dde192e790ada135f7e45a86ab5d231d1713d21a
Hayes <zangetsuebaw@gmail.com>
2024-07-23 12:16:13 +0300

    Update
    Documentation updated

--- docs/search/defines_1.js
+++ docs/search/defines_1.js
@@ -1,8 +1,10 @@
 var searchData=
 [
-  ['json_5fmax_5farray_5felements_0',['JSON_MAX_ARRAY_ELEMENTS',['../json_8h.html#a0b8a743e63f11dddb947f1ba8d7e3163',1,'json.h']]],
-  ['json_5fmax_5fpool_5fsize_1',['JSON_MAX_POOL_SIZE',['../json_8h.html#a5d4c92979db8307b1557a792e017ea3f',1,'json.h']]],
-  ['json_5fmax_5fproperties_2',['JSON_MAX_PROPERTIES',['../json_8h.html#adef5d7b389e673c252abf8dd28a0baa9',1,'json.h']]],
-  ['jvalue_5fget_3',['JVALUE_GET',['../json_8h.html#a704aca00c947eb20546919da51221fb1',1,'json.h']]],
-  ['jvalue_5fref_5fget_4',['JVALUE_REF_GET',['../json_8h.html#a82144766a9c32d569d5ecc288d646c47',1,'json.h']]]
+  ['json_5findent_0',['JSON_INDENT',['../json_8h.html#a275281b7c09df70b07fb718253d6a39d',1,'json.h']]],
+  ['json_5findent_5fnull_1',['JSON_INDENT_NULL',['../json_8h.html#a257659afb0ca01d48b4cc0e834388fec',1,'json.h']]],
+  ['json_5fmax_5farray_5felements_2',['JSON_MAX_ARRAY_ELEMENTS',['../json_8h.html#a0b8a743e63f11dddb947f1ba8d7e3163',1,'json.h']]],
+  ['json_5fmax_5fpool_5fsize_3',['JSON_MAX_POOL_SIZE',['../json_8h.html#a5d4c92979db8307b1557a792e017ea3f',1,'json.h']]],
+  ['json_5fmax_5fproperties_4',['JSON_MAX_PROPERTIES',['../json_8h.html#adef5d7b389e673c252abf8dd28a0baa9',1,'json.h']]],
+  ['jvalue_5fget_5',['JVALUE_GET',['../json_8h.html#a704aca00c947eb20546919da51221fb1',1,'json.h']]],
+  ['jvalue_5fref_5fget_6',['JVALUE_REF_GET',['../json_8h.html#a82144766a9c32d569d5ecc288d646c47',1,'json.h']]]
 ];
